3ffc3190de0a7db3c21c3ddfaef1a0aab5483a38
Piyush <irock0093@gmail.com>
2022-02-12 01:13:07 +0530

    made the photocard responsive

--- src/features/Home/HomePage.js
+++ src/features/Home/HomePage.js
@@ -1,28 +1,23 @@
 import React from "react";
 import PhotoCard from "../PhotoCard/PhotoCard";
+import Footer from "../Footer/Footer";
 import UserStory from "../UserStory/UserStoryContainer";
 import data from "../../data";
+import "./HomePage.scss";
 
 export default function HomePage() {
   const photoCardData = data.photoCardData;
+
+  const photoCardRender = photoCardData.map((photoCard) => {
+    return <PhotoCard {...photoCard} />;
+  });
+
   return (
     <>
-      HELLO FROM HOMEPAGE
+      {photoCardRender}
+      <h1 className="hp__title">Share with us!</h1>
       <UserStory page={"home"} isDesktop={true} />
-      {/* <PhotoCard textTheme={"light"} textSide={"right"} />
-      <PhotoCard textTheme={"dark"} textSide={"left"} /> */}
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <br></br>
-      <h1 style={{ textAlign: "center" }}>Share with us!</h1>
-      {photoCardData.map((photoCard) => {
-        return <PhotoCard {...photoCard} />;
-      })}
+      <Footer />
     </>
   );
 }
