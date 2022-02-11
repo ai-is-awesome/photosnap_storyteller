1c8bd2efa001130dc50af5c5a9cfcd3244006a84
unknown <sabhyankar918@gmail.com>
2022-02-12 02:27:49 +0530

    added pages in routes and stories component in stories page

---
+++ src/features/pages/StoriesPage/StoriesPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserStory from '../../UserStory/UserStoryContainer'
+
+export default function StoriesPage() {
+  return (
+    <>
+      <UserStory page={"stories"} />
+    </>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import { BrowserRouter } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import App from "./App";
 import HomePage from "./features/Home/HomePage";
+import FeaturesPage from "./features/pages/FeaturesPage/FeaturesPage";
+import PricingPage from "./features/pages/PricingPage/PricingPage";
+import StoriesPage from "./features/pages/StoriesPage/StoriesPage";
 import "./stylesheets/main.scss";
 
 
@@ -15,6 +18,9 @@ ReactDOM.render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<HomePage />} />
+          <Route path="stories" element={<StoriesPage />} />
+          <Route path="features" element={<FeaturesPage />} />
+          <Route path="pricing" element={<PricingPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
