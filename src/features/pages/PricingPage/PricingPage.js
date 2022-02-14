6ce3bbe32da07aba64575bfdff7b96385f8af7d2
unknown <sabhyankar918@gmail.com>
2022-02-14 13:52:12 +0530

    added pricing compare table

--- src/features/pages/PricingPage/PricingPage.js
+++ src/features/pages/PricingPage/PricingPage.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import { useMediaQuery } from 'react-responsive'
+import Compare from './Compare'
 
 export default function PricingPage() {
+  const isDesktop = useMediaQuery({
+    query: '(min-width: 1224px)'
+  })
+
   return (
-    <div>PricingPage</div>
+    <>
+      <Compare desktop={isDesktop} />
+    </>
   )
 }
