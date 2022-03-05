d797400544d7bb6760de426f0956445f922c83fc
unknown <sabhyankar918@gmail.com>
2022-03-05 22:59:20 +0530

    added cool animations

--- src/features/Home/HomePage.js
+++ src/features/Home/HomePage.js
@@ -1,24 +1,39 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import PhotoCard from "../PhotoCard/PhotoCard";
 import Footer from "../Footer/Footer";
 import UserStory from "../UserStory/UserStoryContainer";
 import data from "../../data";
 import "./HomePage.scss";
 import Features from "../Fetures/Features";
+import { motion } from "framer-motion";
 
-export default function HomePage() {
+export default function HomePage(props) {
+  const [load , setLoad] = useState(true)
+  useEffect(() => {
+    const animate = setTimeout(() => {
+       setLoad(false)
+    }, 500)
+  
+    return () => {
+      clearTimeout(animate)
+    }
+  }, [])
+
   const photoCardData = data.photoCardData;
-
   const photoCardRender = photoCardData.map((photoCard) => {
     return <PhotoCard {...photoCard} />;
   });
 
   return (
     <>
-      {photoCardRender}
-      <UserStory page="home" />
-      <Features page="home" />
-      <Footer />
+      <section className="hp__container">
+        <motion.div initial={{x:"-30vw"}} animate={load ? {x:0}:{x:0}}>
+        {photoCardRender}
+        <UserStory page="home" />
+        <Features page="home" />
+        <Footer />
+        </motion.div>
+      </section>
     </>
   );
 }
--- src/features/pages/StoriesPage/StoriesPage.js
+++ src/features/pages/StoriesPage/StoriesPage.js
@@ -1,20 +1,37 @@
-import React, { useState } from 'react'
+import React, { useState,useEffect } from 'react'
 import UserStory from '../../UserStory/UserStoryContainer'
 import USTopCard from '../../UserStory/USTopCard'
 import AddStory from './AddStory'
 import StoryForm from './StoryForm/StoryForm'
+import style from "./StoriesPage.module.scss"
+import { motion } from 'framer-motion'
 
-export default function StoriesPage() {
+export default function StoriesPage(props) {
   const [storyForm, setStoryForm] = useState(false)
+  const [load , setLoad] = useState(true)
+  useEffect(() => {
+    const animate = setTimeout(() => {
+       setLoad(false)
+    }, 500)
+  
+    return () => {
+      clearTimeout(animate)
+    }
+  }, [])
 
   return (
     <>
-      <USTopCard />
-      <UserStory page="stories" />
+      <motion.section 
+      initial={{x:"-30vw"}}
+      animate={load ? { x:0 }: { x:0 }}
+      className={style.sp__container}>
+        <USTopCard />
+        <UserStory StoryPageData={props.StoryPageData} page="stories" />
+      </motion.section>
       <AddStory setStoryForm={setStoryForm} />
       {storyForm ? 
-      <StoryForm storyForm={storyForm} setStoryForm={setStoryForm}/> 
-      : ""
+        <StoryForm storyForm={storyForm} setStoryForm={setStoryForm}/> 
+        : ""
       }
     </>
   )
